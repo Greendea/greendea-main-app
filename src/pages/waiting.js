06aa9938a38e8cb87a474f2070e1e12b69c2ec08
Phạm Cao Sang <pcs@netcompany.com>
2023-03-05 22:17:03 +0700

    add content for waiting

--- src/pages/waiting.js
+++ src/pages/waiting.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 function Waiting() {
+    const router = useRouter()
+    const { id } = useSelector(state => state.user)
+    useEffect(() => {
+        id && router.push("/")
+    }, [id])
     return (
         <div className='waiting-wrapper'>
             <Head>
@@ -13,7 +20,8 @@ function Waiting() {
                 <form>
                     <div class="title">
                         <img src="/logowaiting.png" />
-                        <h1>Please wait for the admin to set your role and department!</h1>
+                        <h1>Please wait for the admin to set your role and department.</h1>
+                        <h1>Come back later!</h1>
                     </div>
                     <div class="dot-typing"></div>
                 </form>
