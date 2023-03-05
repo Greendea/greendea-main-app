4bc0567f581b7ba5ae008aa30a3e0d78b0e1cf4c
Phạm Cao Sang <pcs@netcompany.com>
2023-03-05 22:07:48 +0700

    ud

--- src/pages/waiting.js
+++ src/pages/waiting.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Waiting() {
     return (
-        <div>waiting admin for assigning your role</div>
+        <div className='waiting-wrapper'>
+            <div class="waiting">
+                <form>
+                    <div class="title">
+                        <img src="/logowaiting.png" />
+                        <h1>Please wait for the admin to set your role and department!</h1>
+                    </div>
+                    <div class="dot-typing"></div>
+                </form>
+            </div>
+        </div>
     )
 }
 
