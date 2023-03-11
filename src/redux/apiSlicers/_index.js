b4bc6b1f456ed5db639b96c284d6e3570c636e5f
Phạm Cao Sang <pcs@netcompany.com>
2023-03-11 23:39:48 +0700

    ud

--- src/redux/apiSlicers/_index.js
+++ src/redux/apiSlicers/_index.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.BE_URL + '/api',
+        baseUrl: process.env.BE_URL + 'api',
     }),
     tagTypes: ["User", "Department", "Term", "Role", "Topic", "Announcement", "Idea", "Category", "Comment"],
     endpoints: () => ({}),
