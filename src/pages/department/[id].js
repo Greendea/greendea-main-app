32a3a4abb2b8822987a5055d0e5d2c0b0a2cb7d5
Phạm Cao Sang <pcs@netcompany.com>
2023-03-14 10:39:57 +0700

    ud

--- src/pages/department/[id].js
+++ src/pages/department/[id].js
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import React from "react"
 import Layout from "../../components/Layout/Index"
 
-export default function Department({ department, dep_id }) {
+export default function Department() {
     // const router = useRouter()
     // const { id } = router.query
     const { data, isLoading, isSuccess } = useGetDepartmentByIdQuery(dep_id, {
@@ -44,32 +44,32 @@ export default function Department({ department, dep_id }) {
 
 
 
-export async function getStaticPaths() {
-    const res = await fetch(`${process.env.BE_URL}api/department`)
-    const deps = await res.json()
-    console.log(deps)
+// export async function getStaticPaths() {
+//     const res = await fetch(`${process.env.BE_URL}api/department`)
+//     const deps = await res.json()
+//     console.log(deps)
 
-    // Get the paths we want to pre-render based on posts
-    const paths = deps.map((dep) => ({
-        params: { id: dep.id },
-    }))
-    return {
-        paths,
-        fallback: 'blocking', // can also be true or 'blocking'
-    }
-}
+//     // Get the paths we want to pre-render based on posts
+//     const paths = deps.map((dep) => ({
+//         params: { id: dep.id },
+//     }))
+//     return {
+//         paths,
+//         fallback: 'blocking', // can also be true or 'blocking'
+//     }
+// }
 
-// `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps({ params }) {
-    const res = await fetch(`${process.env.BE_URL}api/department/${params.id}`)
-    const department = await res.json()
+// // `getStaticPaths` requires using `getStaticProps`
+// export async function getStaticProps({ params }) {
+//     const res = await fetch(`${process.env.BE_URL}api/department/${params.id}`)
+//     const department = await res.json()
 
-    // Pass post data to the page via props
-    return {
-        props: {
-            department: department,
-            dep_id: params.id
-        },
-        revalidate: 10
-    }
-}
+//     // Pass post data to the page via props
+//     return {
+//         props: {
+//             department: department,
+//             dep_id: params.id
+//         },
+//         revalidate: 10
+//     }
+// }
