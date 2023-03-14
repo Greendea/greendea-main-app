14187465b41e55e5ebe5f6feef3a5a9e725447bd
Phạm Cao Sang <pcs@netcompany.com>
2023-03-14 11:16:36 +0700

    ud

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { uploadImage } from "@/lib/cloudinary"
+import { uploadImage } from "../../lib/cloudinary"
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
