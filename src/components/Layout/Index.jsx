5ebc3b8b129f658796999de23eb31cd2495c3815
Phạm Cao Sang <pcs@netcompany.com>
2023-04-10 12:42:29 +0700

    fix navb text

--- src/components/Layout/Index.jsx
+++ src/components/Layout/Index.jsx
@@ -24,7 +24,7 @@ const menu = (departments) => {
         },
         {
             key: "Sub2",
-            label: <><span>Top Ideas and Latest Comments</span></>,
+            label: <><span>Top and Latest Ideas</span></>,
             icon: <FcStatistics />,
             children: [
                 {
